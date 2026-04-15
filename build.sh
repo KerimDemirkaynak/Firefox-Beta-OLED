aba6175acd3b9eb2ece092905d9c1dd4890ef8fc
Kerim Demirkaynak <76594465+KerimDemirkaynak@users.noreply.github.com>
2026-04-16 02:47:27 +0300

    Update build script for fenix APK processing

--- build.sh
+++ build.sh
@@ -4,24 +4,24 @@ set -e
 
 # Decompile with Apktool (decode resources + classes)
 wget -q https://github.com/iBotPeaches/Apktool/releases/download/v2.11.0/apktool_2.11.0.jar -O apktool.jar
-java -jar apktool.jar d iceraven.apk -o iceraven-patched  # -s flag removed
-rm -rf iceraven-patched/META-INF
+java -jar apktool.jar d fenix.apk -o fenix-patched
+rm -rf fenix-patched/META-INF
 
 # Color patching
-sed -i 's/<color name="fx_mobile_surface">.*/<color name="fx_mobile_surface">#ff000000<\/color>/g' iceraven-patched/res/values-night/colors.xml
-sed -i 's/<color name="fx_mobile_background">.*/<color name="fx_mobile_background">#ff000000<\/color>/g' iceraven-patched/res/values-night/colors.xml
-sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' iceraven-patched/res/values-night/colors.xml
+sed -i 's/<color name="fx_mobile_surface">.*/<color name="fx_mobile_surface">#ff000000<\/color>/g' fenix-patched/res/values-night/colors.xml
+sed -i 's/<color name="fx_mobile_background">.*/<color name="fx_mobile_background">#ff000000<\/color>/g' fenix-patched/res/values-night/colors.xml
+sed -i 's/<color name="fx_mobile_layer_color_2">.*/<color name="fx_mobile_layer_color_2">@color\/photonDarkGrey90<\/color>/g' fenix-patched/res/values-night/colors.xml
 
-# Smali patching
-sed -i 's/ff2b2a33/ff000000/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff42414d/ff15141a/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
-sed -i 's/ff52525e/ff15141a/g' iceraven-patched/smali_classes2/mozilla/components/ui/colors/PhotonColors.smali
+# Smali patching (Klasör numarasından bağımsız olarak dinamik bulma)
+find fenix-patched/smali* -name "PhotonColors.smali" -exec sed -i 's/ff2b2a33/ff000000/g' {} +
+find fenix-patched/smali* -name "PhotonColors.smali" -exec sed -i 's/ff42414d/ff15141a/g' {} +
+find fenix-patched/smali* -name "PhotonColors.smali" -exec sed -i 's/ff52525e/ff15141a/g' {} +
 
 # Recompile the APK
-java -jar apktool.jar b iceraven-patched -o iceraven-patched.apk --use-aapt2
+java -jar apktool.jar b fenix-patched -o fenix-patched-unsigned.apk --use-aapt2
 
 # Align and sign the APK
-zipalign 4 iceraven-patched.apk iceraven-patched-signed.apk
+zipalign 4 fenix-patched-unsigned.apk fenix-patched-signed.apk
 
 # Clean up
-rm -rf iceraven-patched iceraven-patch ed.apk
+rm -rf fenix-patched fenix-patched-unsigned.apk
